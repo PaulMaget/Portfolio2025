099a65fe6dcedb19a0c2c2b259bc1f4e3bf2749c
Paul MAGET <paul.pj.maget@gmail.com>
2025-05-05 16:19:09 +0200

    Amélioration du style de la page principale et ajout de tooltip aux cercles des compétences

---
+++ Portfolio2025/Model/Experience.cs
@@ -0,0 +1,27 @@
+﻿namespace Portfolio2025.Model;
+
+public record Experience
+{
+    public required string Title { get; init; }
+    public required string Organization { get; init; }
+    public required string Year { get; init; }
+    public required string ShortHtmlDescription { get; init; }
+    public required string HtmlDescription { get; init; }
+}
+
+public static class Experiences
+{
+    public static Experience StageMJC => new() {
+        Title = "...",
+        Organization = "MJC Manosque",
+        Year = "2022",
+        ShortHtmlDescription = """
+        <p>
+            
+        </p>
+        """,
+        HtmlDescription = """
+
+        """
+    };
+}
