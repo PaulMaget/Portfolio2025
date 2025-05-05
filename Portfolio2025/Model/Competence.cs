96ea5deb4cbbef22ef0e5aa5613fc07e73f21350
Paul MAGET <paul.pj.maget@gmail.com>
2025-05-05 16:46:04 +0200

    Ajout d'un modèle pour les compétences

---
+++ Portfolio2025/Model/Competence.cs
@@ -0,0 +1,38 @@
+﻿using MudBlazor;
+
+namespace Portfolio2025.Model;
+
+public record Competence
+{
+	public required string Name { get; set; }
+	public required string Logo { get; set; }
+	public required string Description { get; set; }
+	public required CompetenceCategory Category { get; set; }
+}
+
+public enum CompetenceCategory
+{
+	Languages,
+	Frameworks
+}
+
+public static class CompetenceExtensions
+{
+	public static string? ToString(this CompetenceCategory category) {
+		return category switch {
+			CompetenceCategory.Languages => "Langages",
+			CompetenceCategory.Frameworks => "Frameworks",
+			_ => null
+		};
+	}
+}
+
+public static class Competences
+{
+	public static Competence CSharp => new() {
+		Name = "C#",
+		Logo = Icons.Custom.FileFormats.FileImage,
+		Description = "Description",
+		Category = CompetenceCategory.Languages,
+	};
+}
