2d68fd375ec20edd6a586353f0c653945e1932a7
PaulMaget <paul.pj.maget@gmail.com>
2025-06-28 18:22:24 +0200

    Ajout des compétences sur les expériences. Amélioration du style

--- Portfolio2025/Model/Competence.cs
+++ Portfolio2025/Model/Competence.cs
@@ -5,16 +5,18 @@ namespace Portfolio2025.Model;
 
 public record Competence
 {
-	public required string Name { get; set; }
+    public required int Id { get; set; }
+    public required string Name { get; set; }
 	public required string Logo { get; set; }
-	public required string Description { get; set; }
+	public required string HtmlDescription { get; set; }
 	public required CompetenceCategory Category { get; set; }
 }
 
 public enum CompetenceCategory
 {
 	Languages,
-	Frameworks
+	Frameworks,
+    Tools,
 }
 
 public static class CompetenceExtensions
@@ -23,6 +25,7 @@ public static class CompetenceExtensions
 		return category switch {
 			CompetenceCategory.Languages => "Langages",
 			CompetenceCategory.Frameworks => "Frameworks",
+			CompetenceCategory.Tools => "Outils",
 			_ => ""
 		};
 	}
@@ -30,20 +33,23 @@ public static class CompetenceExtensions
 
 public static class Competences
 {
-	public static Competence[] All => [CSharp, CSharp, JavaScript, Angular, CSharp, CSharp, CSharp, CSharp, CSharp];
+	public static Competence[] All => [CSharp, JavaScript, Angular, Unity, CSharp, CSharp, CSharp, CSharp];
 
-	public static Competence CSharp => new() {
-		Name = "C#",
+	public static Competence CSharp => new()
+	{
+		Id = 0,
+        Name = "C#",
 		Logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Logo_C_sharp.svg/1200px-Logo_C_sharp.svg.png",
-		Description = "Le langage haut niveau, orienté objet de Microsoft,<br>l'entreprise américaine notamment responsable de Flight Simulator 2020.",
+		HtmlDescription = "Le langage haut niveau, orienté objet de Microsoft,<br>l'entreprise américaine notamment responsable de Flight Simulator 2020.",
 		Category = CompetenceCategory.Languages,
 	};
 
 	public static Competence JavaScript => new()
 	{
-		Name = "JavaScript",
+		Id = 1,
+        Name = "JavaScript",
 		Logo = "https://lh3.googleusercontent.com/proxy/nuWxyZIEBmlwkLPiIpq_bJ4eXDw9uozT2D6WSBxvR4NYB3bMZ9T1TrYNao1zQL9MivRNV33D9QeJiDbVManBCLt2Yw8muTBeeOF3ghlbhT2NiQFFScHqgFbwGqFMTKHop6gMf6jQz9kqkPH42mdpaJcx",
-        Description = """
+        HtmlDescription = """
 			P poeifhj qespoihjf qpeoihfj qe f
 			fspeiqh epfih q.<br />
 			<br>
@@ -57,9 +63,10 @@ public static class Competences
 
 	public static Competence Angular => new()
 	{
-		Name = "Angular",
+		Id = 2,
+        Name = "Angular",
 		Logo = "https://brandlogos.net/wp-content/uploads/2025/04/angular_icon-logo_brandlogos.net_jn7wi-512x542.png",
-		Description = """
+		HtmlDescription = """
 			P poeifhj qespoihjf qpeoihfj qe f
 			fspeiqh epfih q.<br />
 			<br />
@@ -67,4 +74,18 @@ public static class Competences
 			""",
 		Category = CompetenceCategory.Frameworks,
 	};
+
+    public static Competence Unity => new()
+    {
+		Id = 3,
+        Name = "Unity",
+        Logo = "https://cdn-icons-png.flaticon.com/512/5969/5969346.png",
+        HtmlDescription = """
+			P poeifhj qespoihjf qpeoihfj qe f
+			fspeiqh epfih q.<br />
+			<br />
+			fqiueh efliquh efliuh.
+			""",
+        Category = CompetenceCategory.Tools,
+    };
 }
