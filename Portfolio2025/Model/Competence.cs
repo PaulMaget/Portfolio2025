cd95996585df7b71b06b7935f8918ccb0bb60b66
PaulMaget <paul.pj.maget@gmail.com>
2025-06-30 19:02:54 +0200

    Ajout d'Alpilink. Ajout de HardSkills et de SoftSkills.

--- Portfolio2025/Model/Competence.cs
+++ Portfolio2025/Model/Competence.cs
@@ -15,6 +15,7 @@ public enum CompetenceCategory
 	Languages,
 	Frameworks,
     Tools,
+    Other,
 }
 
 public static class CompetenceExtensions
@@ -24,7 +25,8 @@ public static class CompetenceExtensions
 			CompetenceCategory.Languages => "Langages",
 			CompetenceCategory.Frameworks => "Frameworks",
 			CompetenceCategory.Tools => "Outils",
-			_ => ""
+			CompetenceCategory.Other => "Autres technologies",
+			_ => "",
 		};
 	}
 
@@ -37,19 +39,19 @@ public static class CompetenceExtensions
 
 public static class Competences
 {
-	public static List<Competence> All => [CSharp, JavaScript, Php, AspNet, Angular, Symfony, Unity];
+	public static List<Competence> All => [CSharp, JavaScript, Php, AspNet, Angular, Symfony, Unity, RabbitMq, SignalR];
 
-	public static Competence CSharp { get; } = new()
-	{
+    public static Competence CSharp { get; } = new()
+    {
         Name = "C#",
-		Logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Logo_C_sharp.svg/1200px-Logo_C_sharp.svg.png",
-		HtmlDescription = """
-			J'ai initialement appris le C# en autodidacte pour utiliser Unity.<br>
-			Je suis activement le développement du langage et de l'écosystème .Net.<br>
-			J'ai eu l'occasion de travailler professionnellement avec C# dans le cadre de mon stage et de mon alternance de BUT.
-			""",
+        Logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Logo_C_sharp.svg/1200px-Logo_C_sharp.svg.png",
+        HtmlDescription = """
+            J'ai initialement appris le C# en autodidacte pour utiliser Unity.<br>
+            Je suis activement le développement du langage et de l'écosystème .Net.<br>
+            J'ai eu l'occasion de travailler professionnellement avec C# dans le cadre de mon stage et de mon alternance de BUT.
+            """,
         Category = CompetenceCategory.Languages,
-	};
+    };
 
 	public static Competence JavaScript { get; } = new()
 	{
@@ -96,27 +98,49 @@ public static class Competences
 	};
 
     public static Competence Symfony { get; } = new()
-	{
+    {
         Name = "Symfony",
-		Logo = "https://images.icon-icons.com/2415/PNG/512/symfony_original_wordmark_logo_icon_146328.png",
-		HtmlDescription = """
-			J'ai commencé par apprendre Symfony en autodidacte pour un projet universitaire.<br>
-			J'ai ensuite eu des cours sur ce framework dans le cadre de mon BUT.
-			""",
-		Category = CompetenceCategory.Frameworks,
-	};
+        Logo = "https://images.icon-icons.com/2415/PNG/512/symfony_original_wordmark_logo_icon_146328.png",
+        HtmlDescription = """
+            J'ai commencé à apprendre Symfony en autodidacte pour un projet universitaire.<br>
+            J'ai ensuite eu des cours sur ce framework dans le cadre de mon BUT.
+            """,
+        Category = CompetenceCategory.Frameworks,
+    };
 
     public static Competence Unity { get; } = new()
     {
         Name = "Unity",
         Logo = "https://cdn-icons-png.flaticon.com/512/5969/5969346.png",
         HtmlDescription = """
-			J'apprends et je suis le développement d'Unity depuis 2019.<br>
-			J'ai eu l'occasion de participer à des Game Jams en 2023 et 2024.
-			Les jeux développés sont disponibles sur mon <a class="default-link" href="https://poldev.itch.io/">Itch.io</a>.
-			""",
-			//J'expermiente avec le moteur pour comprendre ses fonctionnalités, ses bonnes pratiques
-			//et je développe des outils visant à améliorer la rapidité de développement.<br>
+            J'apprends et je suis le développement d'Unity depuis 2019.<br>
+            J'ai eu l'occasion de participer à des Game Jams en 2023 et 2024.<br>
+            Les jeux développés sont disponibles sur mon <a class="default-link" href="https://poldev.itch.io/">Itch.io</a>.
+            """,
+        // J'expérimente avec le moteur pour comprendre ses fonctionnalités, ses bonnes pratiques
+        // et je développe des outils visant à améliorer la rapidité de développement.<br>
         Category = CompetenceCategory.Tools,
+    };
+
+    public static Competence RabbitMq { get; } = new()
+    {
+        Name = "RabbitMQ",
+        Logo = "./images/RabbitMQ.png",
+        HtmlDescription = """
+			J’ai appris RabbitMQ pendant mon alternance de BUT.<br>
+			J’ai conçu et implémenté une topologie pour faire communiquer une borne de vente avec un serveur distant.
+			""",
+        Category = CompetenceCategory.Other,
+    };
+
+    public static Competence SignalR { get; } = new()
+    {
+        Name = "SignalR",
+        Logo = "https://sofa1.at/wp-content/uploads/2023/03/SignalR-Logo.png",
+        HtmlDescription = """
+            J’ai appris SignalR pendant mon alternance de BUT.<br>
+            Je l’ai utilisé pour mettre en place une communication par événements entre des applications backend et frontend.
+            """,
+        Category = CompetenceCategory.Other,
     };
 }
--- Portfolio2025/Model/Experience.cs
+++ Portfolio2025/Model/Experience.cs
@@ -9,6 +9,8 @@ public record Experience
     public required string HtmlDescription { get; set; }
     public string? ImageUrl { get; set; }// = "https://placehold.co/50x50";
     public List<Competence> Competences { get; set; } = [];
+    public List<string> HardSkills { get; set; } = [];
+    public List<string> SoftSkills { get; set; } = [];
 }
 
 public static class ExperienceExtensions
@@ -22,16 +24,24 @@ public static class ExperienceExtensions
 
 public static class Experiences
 {
-    public static List<Experience> All => [StageMJC, OrganizingHistory];
+    public static List<Experience> All => [StageMJC, OrganizingHistory, AlternanceAlpilink];
 
-    public static Experience StageMJC { get; } = new() {
-        Name = "Animateur bénévole",
+    public static Experience StageMJC { get; } = new()
+    {
+        Name = "Animation stages jeux vidéo",
         Organization = "MJC Manosque",
         Year = "2022",
         Competences = [
             Competences.Unity,
-            //Competences.Pedagogie,
-            //Competences.GestionDeProjet,
+        ],
+        HardSkills = [
+            "Développement Unity 2D/3D",
+            //"URP",
+        ],
+        SoftSkills = [
+            "Préparation d’un projet",
+            "Pédagogie",
+            "Gestion de projet",
         ],
         ShortHtmlDescription = """
         <p>
@@ -66,8 +76,15 @@ public static class Experiences
             Competences.Php,
             Competences.Symfony,
             Competences.JavaScript,
-            //Competences.GestionDeProjet,
-            //Competences.ConceptionArchitecture,
+        ],
+        HardSkills = [
+            "Développement web fullstack",
+            "Conception d’architecture",
+            "Système d’authentification",
+        ],
+        SoftSkills = [
+            "Travail en équipe",
+            "Gestion de projet",
         ],
         ShortHtmlDescription = """
         <p>
@@ -78,7 +95,7 @@ public static class Experiences
         <p>
             En deuxième année de BUT, il nous a été demandé de développer un jeu ludo-éducatif en ligne.
             Le site devait être sécurisé avec un système d'authentification pour les enseignants et les élèves, avec une interface d'administration pour les enseignants.
-            Mon groupe a choisi d'orienter le jeu vers les cours d'histoire de troisième, pour permettre aux collégiens de réviser leur Brevet d'une manière ludique.
+            Mon groupe a choisi d'orienter le jeu vers les cours d'histoire de troisième, pour permettre aux collégiens de réviser leur Brevet de manière ludique.
         </p>
         <p>
             Le projet était en groupe de huit, pour une durée de trois mois.<br>
@@ -90,9 +107,61 @@ public static class Experiences
         </p>
         <img src="./images/OrganizingHistory_game.png" alt="Capture de l'écran du jeu" style="width: 50%" />
         <p>
-            Notre projet a remporté le 3ème prix du meilleur prototype parmi tous les projets de la promotion. À la suite d'un vote de la promotion, nous avons aussi remporté le prix du public parmi les 3 projets qui étaient présentés.
+            Notre projet a remporté le 3<sup>e</sup> prix du meilleur prototype parmi tous les projets de la promotion. À la suite d'un vote de la promotion, nous avons aussi remporté le prix du public parmi les 3 projets qui étaient présentés.
         </p>
         """,
         ImageUrl = "./images/OrganizingHistory_home.png",
+    };
+
+    public static Experience AlternanceAlpilink { get; } = new()
+    {
+        Name = "Développement backend borne",
+        Organization = "Alpilink",
+        Year = "2024-2025",
+        Competences = [
+            Competences.CSharp,
+            Competences.AspNet,
+            Competences.RabbitMq,
+            Competences.SignalR,
+        ],
+        HardSkills = [
+            "Backend C#",
+            "Conception d’architecture",
+            "Communication Serial Port",
+            "Communication SignalR",
+            "Communication RabbitMQ",
+            "Développement d’un framework de test",
+        ],
+        SoftSkills = [
+            "Travail en équipe",
+            "Méthode agile",
+            "Prise d’initiative",
+            "Rédaction de documentation",
+        ],
+        ShortHtmlDescription = """
+        <p>
+            Développement de l’application backend pour le nouveau système de bornes de vente d’Alpilink.
+        </p>
+        """,
+        HtmlDescription = """
+        <p>
+            En BUT, j’ai pu faire un stage puis une alternance d'un an dans l’entreprise <a class="default-link" href="https://www.alpilink.fr/">Alpilink</a>,
+            spécialisée dans la vente de forfaits en ligne pour les stations de ski.
+        </p>
+        <p>
+            Ma mission était de concevoir, développer et documenter une nouvelle application backend pour les bornes de vente de l’entreprise.
+            Ces bornes sont principalement vendues à des stations de ski pour permettre une vente et une distribution de forfaits automatiques.
+        </p>
+        <img src="./images/Kiosk-light.jpg" alt="Photo d'une borne" style="width: 50%" />
+        <p>
+            Je suis intervenu sur la partie backend de l'application, qui fait l'interface entre l'application visuelle et les périphériques de la borne par Serial Port.
+            Le backend a été écrit en C# avec le framework ASP .Net et utilise des technologies comme RabbitMQ et SignalR pour communiquer avec l’application frontend et un serveur distant.
+        </p>
+        <p>
+            En raison des sommes d'argent en jeu, j’ai été particulièrement prudent lors de la communication avec le module monétique et pour la gestion des erreurs. Pour cette raison, j'ai développé un framework de test permettant aux testeurs de simuler des périphériques matériels.
+        </p>
+        <img src="./images/borne-click-and-collect.jpg" alt="Photo d'une borne" style="width: 50%" />
+        """,
+        ImageUrl = "./images/Kiosk-light.jpg",
     };
 }
